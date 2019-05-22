f40b7545816d13ba5e06a53a857854f2bd37f01e
Markus Pettersson <markus.pettersson1998@gmail.com>
2019-05-22 21:12:56 +0200

    Added Clojure to the soup :-dD

---
+++ textprocessing.clj
@@ -0,0 +1,54 @@
+(require '[clojure.string :as string])
+
+(def book
+  (slurp "red_room.txt"))
+
+(defn words [text]
+  (string/replace text
+    #"[\W]{1,}"
+    " "))
+
+(defn split-at-whitespace [text]
+  (string/split text #"\s+"))
+
+(defn clean-text [text]
+  (-> text
+    (words)
+    (string/lower-case)
+    (string/trim)))
+
+(defn count-words [text]
+  (count 
+    (split-at-whitespace 
+      (clean-text text))))
+
+;; This function is cool 
+(defn group-unique-words [text]
+  (-> text
+    (clean-text)
+    (split-at-whitespace)
+    (frequencies)
+    (#(sort-by second %)))) ;; Sort by values of map
+
+
+;; ----------- VARIABLES -----------
+(def unique-words-in-book
+  (group-unique-words book))
+
+;; Count all words in the book
+(def word-count-1
+  (count-words book))
+
+;; Count all words by summing all occurences of all
+;; unique words
+(def word-count-2
+  (reduce + 
+    (map second unique-words-in-book)))
+
+;; Check if they are the same!
+(= word-count-1 word-count-2)
+
+;; Map of all the unique words. 
+;; Key = word
+;; Value = Number of times the words occurences
+(println unique-words-in-book)
